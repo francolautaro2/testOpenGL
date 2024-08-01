9b7f31d89eed7127ec4f20468cacc555b9f6ef5a
franco <francooro322@gmail.com>
2024-08-01 16:55:27 -0300

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -53,18 +53,18 @@ void main()
 }
 )";
 
-// Funciones de ayuda
+
 void checkShaderCompileStatus(unsigned int shader, const std::string& type);
 void checkProgramLinkStatus(unsigned int program);
 
 int main() {
-    // Inicialización de GLFW
+    
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Crear una ventana GLFW
+    
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL) {
         std::cerr << "Failed to create GLFW window" << std::endl;
@@ -76,16 +76,16 @@ int main() {
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
 
-    // Capturar el mouse
+    
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-    // Inicializar GLAD
+    
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cerr << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    // Compilar y vincular shaders
+    
     unsigned int vertexShader, fragmentShader, shaderProgram;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -106,17 +106,16 @@ int main() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    // Definir los datos del triángulo y configurar los apuntadores de los atributos
-    float vertices[] = {
-        // posiciones         
-         0.5f,  0.5f, 0.0f,  // arriba derecha
-         0.5f, -0.5f, 0.0f,  // abajo derecha
-        -0.5f, -0.5f, 0.0f,  // abajo izquierda
-        -0.5f,  0.5f, 0.0f   // arriba izquierda 
+    
+    float vertices[] = {      
+         0.5f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,  
+        -0.5f, -0.5f, 0.0f,  
+        -0.5f,  0.5f, 0.0f   
     };
-    unsigned int indices[] = {  // nota que comienza desde 0!
-        0, 1, 3,   // primer triángulo
-        1, 2, 3    // segundo triángulo
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
     };
 
     unsigned int VBO, VAO, EBO;
@@ -124,77 +123,76 @@ int main() {
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
 
-    // Primero vinculamos el VAO
+    
     glBindVertexArray(VAO);
 
-    // Luego vinculamos y configuramos el VBO
+    
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // Luego vinculamos y configuramos el EBO
+    
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // Configurar el apuntador de los atributos de los vértices
+   
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // Note que esto es permitido, el llamado a glVertexAttribPointer registra VBO como el buffer de vértices vinculado al atributo de vértice actualmente vinculado
-    // así que después podemos desvincular con seguridad
+    
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
 
-    // Recordamos: NO DESVINCULAR EL EBO ANTES DE DESVINCULAR EL VAO, porque el VAO recuerda las configuraciones de los vínculos de los elementos buffer 
+     
     glBindVertexArray(0); 
 
-    // Render loop
+   
     while (!glfwWindowShouldClose(window)) {
-        // Calcular el tiempo delta
+       
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
-        // Input
+        
         processInput(window);
 
-        // Render
+        
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Activar el shader
+        
         glUseProgram(shaderProgram);
 
-        // Crear transformaciones
+      
         glm::mat4 view = camera.GetViewMatrix();
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
-        // Obtener las ubicaciones de las matrices
+        
         unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
         unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
 
-        // Pasar las matrices a los shaders
+        
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-        // Renderizar el triángulo
+        
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         
-        // Intercambiar buffers y procesar eventos
+     
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // Opcional: desalocar todos los recursos una vez que hayan dejado de ser necesarios
+    
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
 
-    // Terminar GLFW, liberando cualquier recurso asignado por GLFW
+    
     glfwTerminate();
     return 0;
 }
 
-// Procesar todas las entradas: consultar GLFW si las teclas relevantes están presionadas/liberadas en este frame y reaccionar en consecuencia
+
 void processInput(GLFWwindow *window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
@@ -207,13 +205,10 @@ void processInput(GLFWwindow *window) {
     camera.ProcessKeyboard(deltaTime, forward, backward, left, right);
 }
 
-// GLFW: cada vez que el tamaño de la ventana cambia (por el sistema operativo o redimensionamiento del usuario), se ejecuta esta función de devolución de llamada
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    // Asegúrate de que la vista coincida con las nuevas dimensiones de la ventana; ten en cuenta que el ancho y la altura serán significativamente mayores de lo especificado en las pantallas retina.
     glViewport(0, 0, width, height);
 }
 
-// GLFW: cada vez que el ratón se mueve, se llama a esta función de devolución de llamada
 void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
     if (firstMouse) {
         lastX = xpos;
@@ -222,7 +217,7 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
     }
 
     float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; // invertido ya que las coordenadas y van de abajo hacia arriba
+    float yoffset = lastY - ypos;
 
     lastX = xpos;
     lastY = ypos;
@@ -230,12 +225,10 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
     camera.ProcessMouseMovement(xoffset, yoffset);
 }
 
-// GLFW: cada vez que la rueda de desplazamiento del ratón se desplaza, se llama a esta función de devolución de llamada
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
     camera.ProcessMouseScroll(yoffset);
 }
 
-// Función para verificar el estado de compilación del shader
 void checkShaderCompileStatus(unsigned int shader, const std::string& type) {
     int success;
     char infoLog[1024];
@@ -246,7 +239,6 @@ void checkShaderCompileStatus(unsigned int shader, const std::string& type) {
     }
 }
 
-// Función para verificar el estado de enlace del programa
 void checkProgramLinkStatus(unsigned int program) {
     int success;
     char infoLog[1024];
